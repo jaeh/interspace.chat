2486be22290b7f817b49ef8096e7b40f65ef5142
geleeroyale <krassvs@gmail.com>
2020-04-20 10:45:40 +0200

    put global styles in index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Global, css } from "@emotion/core";
 import { BrowserView } from "react-device-detect";
 
 import Space from "./components/Space";
@@ -13,8 +12,8 @@ import Header from "./components/Header";
 const Wrapper = styled("div")`
   color: ${(props) => props.theme.body};
   background-color: ${(props) => props.theme.background};
-  width: 100vw;
-  height: 100vh;
+  max-width: 100vw;
+  min-height: 100vh;
 
   a {
     color: ${(props) => props.theme.highlight};
@@ -31,30 +30,7 @@ function App() {
           </BrowserView>
           <Space />
         </SpaceContext>
-        <Global
-          styles={css`
-            * {
-              margin: 0px;
-              padding: 0px;
-              font-family: "Helvetica Neue", Arial, sans-serif;
-              font-weight: 300;
-              font-size: 14px;
-            }
-            body {
-              margin: 0px;
-              padding: 0px;
-            }
-            .click-zone {
-              transition: 1s cubic-bezier(0.2, 0.8, 0.2, 1);
 
-              & :hover {
-                text-shadow: 10px 10px 10px black;
-                filter: saturate(0%);
-                cursor: pointer;
-              }
-            }
-          `}
-        />
         <FloatingRoomWindow />
       </RootContextProvider>
     </Wrapper>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Global, css } from "@emotion/core";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 import WebFont from "webfontloader";
@@ -14,6 +15,32 @@ WebFont.load({
 
 ReactDOM.render(
   <ThemeProvider>
+    <Global
+      styles={css`
+        * {
+          margin: 0px;
+          padding: 0px;
+          font-family: "Helvetica Neue", Arial, sans-serif;
+          font-weight: 300;
+          font-size: 14px;
+        }
+        body {
+          margin: 0px;
+          padding: 0px;
+          color: whitesmoke;
+          background-color: black;
+        }
+        .click-zone {
+          transition: 1s cubic-bezier(0.2, 0.8, 0.2, 1);
+
+          & :hover {
+            text-shadow: 10px 10px 10px black;
+            filter: saturate(0%);
+            cursor: pointer;
+          }
+        }
+      `}
+    />
     <App />
   </ThemeProvider>,
   document.getElementById("root")
