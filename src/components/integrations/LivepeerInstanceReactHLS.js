e81ac1d66368aef7b47b76d3d41874bc3e4c46bc
geleeroyale <krassvs@gmail.com>
2020-04-20 22:59:10 +0200

    *

--- src/components/integrations/LivepeerInstanceReactHLS.js
+++ src/components/integrations/LivepeerInstanceReactHLS.js
@@ -13,9 +13,9 @@ function LivepeerInstance() {
   return (
     <LivepeerContainer>
       <iframe
-        width="100%"
-        height="100%"
-        src="https://media.livepeer.org/embed?aspectRatio=16%3A9&maxWidth=100%25&url=http%3A%2F%2F167.172.108.130%3A8935%2Fstream%2Fethturin.m3u8"
+        width="640"
+        height="360"
+        src="http://media.livepeer.org/embed?aspectRatio=16%3A9&maxWidth=100%25&url=http%3A%2F%2F167.172.108.130%3A8935%2Fstream%2Fethturin.m3u8"
         allowfullscreen
       ></iframe>
     </LivepeerContainer>
