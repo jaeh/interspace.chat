8a3cb78be2c2295ddf9c4dfb4286c49a121ddce6
geleeroyale <krassvs@gmail.com>
2020-04-22 02:39:52 +0200

    equalize theme colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,17 +3,18 @@ const black = "#000000";
 const gray = "#e6e7ec";
 const green = "#78FFBE";
 const purple = "#CFA6FF";
+const yellow = "#FCE96A";
 
 const themeLight = {
   background: gray,
   body: black,
-  highlight: purple,
+  highlight: yellow,
 };
 
 const themeDark = {
-  background: black,
-  body: white,
-  highlight: green,
+  background: gray,
+  body: black,
+  highlight: yellow,
 };
 
 const theme = (mode) => (mode === "light" ? themeLight : themeDark);
