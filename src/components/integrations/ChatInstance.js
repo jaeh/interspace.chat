9a5eac3eb309985bc1552a2afce4cc2cb4b87970
jascha <jascha@jaeh.at>
2020-05-16 22:23:56 +0200

    start using const instead of function, move state to separate objects

--- src/components/integrations/ChatInstance.js
+++ src/components/integrations/ChatInstance.js
@@ -1,29 +1,37 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { FloatingSpaceContext } from '../../contexts/FloatingSpaceContext'
 
-function ChatInstance() {
-  const [discordRoom, setDiscordRoom] = useState('692409996083855501')
-  const { currentFloatingSpaces } = useContext(FloatingSpaceContext)
-  const space = currentFloatingSpaces
+const ChatInstance = () => {
+  const [discordRoom, setDiscordRoom] = useState({
+    current: 692409996083855501,
+    spaces: [
+      ['main-room', 692423172020240444],
+      ['lobby', 692423341046366208],
+      ['mentor-ring', 692423431777681488],
+      ['claim poap token', 693135339249991701],
+    ],
+    default: 692409996083855501,
+  })
+  const { currentFloatingSpaces: space } = useContext(FloatingSpaceContext)
 
   useEffect(() => {
-    if (space.indexOf('main-room') > -1) {
-      setDiscordRoom('692423172020240444')
-    } else if (space.indexOf('lobby') > -1) {
-      setDiscordRoom('692423341046366208')
-    } else if (space.indexOf('mentor-ring') > -1) {
-      setDiscordRoom('692423431777681488')
-    } else if (space.indexOf('claim poap token') > -1) {
-      setDiscordRoom('693135339249991701')
-    } else {
-      setDiscordRoom('692409996083855501')
+    let spaceExists = false
+
+    discordRoom.spaces.forEach(([maybeSpace, id]) => {
+      if (space.includes(maybeSpace)) {
+        spaceExists = true
+        setDiscordRoom({ ...discordRoom, current: id })
+      }
+    })
+
+    if (!spaceExists) {
+      setDiscordRoom({ ...discordRoom, current: discordRoom.default })
     }
-    return console.log(discordRoom)
   }, [space, discordRoom])
 
   return (
     <iframe
-      src={`https://titanembeds.com/embed/690315811293888778?css=85&defaultchannel=${discordRoom}&theme=DiscordDark`}
+      src={`https://titanembeds.com/embed/690315811293888778?css=85&defaultchannel=${discordRoom.current}&theme=DiscordDark`}
       width="100%"
       height="100%"
       frameBorder="0"
--- src/components/integrations/YoutubeInstanceWednesday.js
+++ src/components/integrations/YoutubeInstanceWednesday.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect, useContext } from 'react'
 import { FloatingSpaceContext } from '../../contexts/FloatingSpaceContext'
 
 function YoutubeInstance({ roomData }) {
+  const { currentFloatingSpaces: space } = useContext(FloatingSpaceContext)
+
   const [youtubeRoom, setYoutubeRoom] = useState('lhjo2FuU4v0')
-  const { currentFloatingSpaces } = useContext(FloatingSpaceContext)
-  const space = currentFloatingSpaces
 
   useEffect(() => {
     if (space.indexOf('main-room') > -1) {
@@ -12,8 +12,8 @@ function YoutubeInstance({ roomData }) {
     } else {
       setYoutubeRoom('lhjo2FuU4v0')
     }
-    return console.log(youtubeRoom)
   }, [space, youtubeRoom])
+
   return (
     <iframe
       title="Youtube Livestream"
