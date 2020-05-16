8c4eff6f42144ed83b7f53bb69d60e524bcdca94
jascha <jascha@jaeh.at>
2020-05-16 22:34:57 +0200

    Credits: remove return statement

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -50,79 +50,77 @@ const StyledListLink = styled.li`
   grid-gap: 0.5rem;
 `
 
-const Credits = () => {
-  return (
-    <CreditsStyled>
-      <Collapsible trigger="Credits">
-        <p>Art used:</p>
-        <ul>
-          <StyledListItem>
-            "Solidity Summit" graphics by{' '}
-            <a href="https://twitter.com/lililashka" target="_blank" rel="noopener noreferrer">
-              @Lili ✰ ツ
-            </a>{' '}
-          </StyledListItem>
-        </ul>
-        <hr />
-        <p>Tech used:</p>
-        <ul>
-          <StyledListLink>
-            <a href="https://meet.jit.si" target="_blank" rel="noopener noreferrer">
-              <img src={Jitsilogo} width="30px" alt="Jitsi logo"></img>
-            </a>
-            <a href="https://meet.jit.si" target="_blank" rel="noopener noreferrer">
-              Video Conferencing - JitSi Open Source
-            </a>
-          </StyledListLink>
-          <StyledListLink>
-            <a href="https://titanembeds.com" target="_blank" rel="noopener noreferrer">
-              <img src={Titanembeds} width="30px" alt="Titanembeds logo"></img>
-            </a>
-            <a href="https://titanembeds.com" target="_blank" rel="noopener noreferrer">
-              Chat integration - Titan Embeds
-            </a>
-          </StyledListLink>
-        </ul>
-        <hr />
-        <ul>
-          <StyledListItem>
-            Made with{' '}
-            <span role="img" aria-label="heart">
-              💝
-            </span>{' '}
-            by{' '}
-            <a href="https://github.com/interspacechat" target="_blank" rel="noopener noreferrer">
-              interspace.chat
-            </a>
-          </StyledListItem>
-          <StyledListItem>
-            Donate to our interspace.chat{' '}
-            <a
-              href="https://gitcoin.co/grants/516/interspacechat"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Gitcoin Grant
-            </a>{' '}
-          </StyledListItem>
-          <StyledListItem>
-            or directly to{' '}
-            <a
-              href="https://etherscan.io/address/support.interspacechat.eth"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              noncon.eth
-            </a>{' '}
-          </StyledListItem>
-          <StyledListItem>
-            Spread the word
-            <ShareButton />
-          </StyledListItem>
-        </ul>
-      </Collapsible>
-    </CreditsStyled>
-  )
-}
+const Credits = () => (
+  <CreditsStyled>
+    <Collapsible trigger="Credits">
+      <p>Art used:</p>
+      <ul>
+        <StyledListItem>
+          "Solidity Summit" graphics by{' '}
+          <a href="https://twitter.com/lililashka" target="_blank" rel="noopener noreferrer">
+            @Lili ✰ ツ
+          </a>{' '}
+        </StyledListItem>
+      </ul>
+      <hr />
+      <p>Tech used:</p>
+      <ul>
+        <StyledListLink>
+          <a href="https://meet.jit.si" target="_blank" rel="noopener noreferrer">
+            <img src={Jitsilogo} width="30px" alt="Jitsi logo"></img>
+          </a>
+          <a href="https://meet.jit.si" target="_blank" rel="noopener noreferrer">
+            Video Conferencing - JitSi Open Source
+          </a>
+        </StyledListLink>
+        <StyledListLink>
+          <a href="https://titanembeds.com" target="_blank" rel="noopener noreferrer">
+            <img src={Titanembeds} width="30px" alt="Titanembeds logo"></img>
+          </a>
+          <a href="https://titanembeds.com" target="_blank" rel="noopener noreferrer">
+            Chat integration - Titan Embeds
+          </a>
+        </StyledListLink>
+      </ul>
+      <hr />
+      <ul>
+        <StyledListItem>
+          Made with{' '}
+          <span role="img" aria-label="heart">
+            💝
+          </span>{' '}
+          by{' '}
+          <a href="https://github.com/interspacechat" target="_blank" rel="noopener noreferrer">
+            interspace.chat
+          </a>
+        </StyledListItem>
+        <StyledListItem>
+          Donate to our interspace.chat{' '}
+          <a
+            href="https://gitcoin.co/grants/516/interspacechat"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gitcoin Grant
+          </a>{' '}
+        </StyledListItem>
+        <StyledListItem>
+          or directly to{' '}
+          <a
+            href="https://etherscan.io/address/support.interspacechat.eth"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            noncon.eth
+          </a>{' '}
+        </StyledListItem>
+        <StyledListItem>
+          Spread the word
+          <ShareButton />
+        </StyledListItem>
+      </ul>
+    </Collapsible>
+  </CreditsStyled>
+)
 
 export default Credits
